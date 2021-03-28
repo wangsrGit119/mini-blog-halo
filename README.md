753e80474a726d7749d9d030cc412124bf3d943c
wangsrGit119 <l>
2021-03-28 16:44:29 +0800

    评论优化

--- README.md
+++ README.md
@@ -6,8 +6,11 @@
 - 下载后直接微信小程序开发工具打开即可。
 - 在app.js配置acess_key以及线上api地址
 
+## 小程序体验
 
-# Halo开源博客地址
+![苏克分享](https://cdn.jsdelivr.net/gh/wangsrGit119/wangsr-image-bucket/img-article/blog-wxchat-gh_0b089e4e80ed_258.jpg)
+
+## Halo开源博客地址
 
 - [halo官网](https://halo.run/)
 - [halo-GitHub地址](https://github.com/halo-dev/halo)
@@ -16,3 +19,19 @@
 
 - [云开发文档](https://developers.weixin.qq.com/miniprogram/dev/wxcloud/basis/getting-started.html)
 
+## 本地或者自己部署线上配置
+
+- 基础配置
+
+```
+    this.globalData = {
+      baseUrl: 'https://xxxxxx.cn/api', //api
+      api_access_key:"ssssss", //token
+      index_bg_image_url:"https://cdn.jsdelivr.net/gh/wangsrGit119/wangsr-image-bucket/img-article/photo-1507738978512-35798112892c.jfif",//首页背景
+      title:"",//自定义首页title
+    }
+```
+   - baseUrl：基础的API（halo博客的）
+   - api_access_key ：博客后台开启api后设置的token
+   - index_bg_image_url：首页bar背景图片
+   - title ： 首页展示的标题
--- miniprogram/pages/article/article.js
+++ miniprogram/pages/article/article.js
@@ -9,9 +9,15 @@ Page({
    */
   data: {
     articleDetail:{},
+    articleId:undefined,
     showSkeleton:true,
     capsuleBarHeight:app.capsuleBarHeight,//顶部高度
     comments:[],//评论
+    userInfo:app.globalData.userInfo,
+    actionSheetShow:false,
+    myComment:undefined,
+    email:"",
+    currentComment:undefined,//选中的当前评论
 
   },
   
@@ -20,14 +26,16 @@ Page({
    */
   onLoad: function (options) {
     const that = this;
-   
+    
+    const data = JSON.parse(decodeURIComponent(options.item));
     // 骨架屏显示
     setTimeout(function(){
       that.setData({
-        showSkeleton:false
+        showSkeleton:false,
+        articleId:data.id
       })
-    },3000)
-    const data = JSON.parse(decodeURIComponent(options.item));
+    },1000)
+    console.log(data)
     // console.log(data)
     this.loadArticleDetail(data.id);
     this.loadComments(data.id)   
@@ -146,6 +154,119 @@ onShareAppMessage: function (res) {
           that.setData({
             comments:res.data.data.content
           })
+        }
+      },
+      fail: function (res) {
+        console.log("请求异常",res)
+      }
+    })
+  },
+  toComment(e){
+    const that = this;
+    this.setData({
+      currentComment:e.currentTarget.dataset.commentOne
+    })
+    if(app.globalData.userInfo){
+      that.setData({
+        actionSheetShow:true,
+      })
+    }else{
+      wx.showModal({
+        title: '温馨提示',
+        content: '评论需要使用你的微信昵称',
+        success(res) {
+          if (res.confirm) {
+            wx.getUserProfile({
+            desc: "获取你的昵称、头像、地区及性别",
+            success: res => {
+              console.log(res)
+              app.globalData.userInfo = res.userInfo;
+              that.setData({
+                actionSheetShow:true,
+                userInfo:res.userInfo
+              })
+
+            },
+            fail: res => {
+              //拒绝授权
+              return;
+            }
+          })} else if (res.cancel) {
+            //拒绝授权 showErrorModal是自定义的提示
+            return;
+          }
+        }
+      })
+    }
+    
+  },
+  onClickHide(e){
+    this.setData({actionSheetShow : false});
+  },
+  saveEvent(e){
+    if(this.data.myComment !== undefined 
+      && this.data.email !== undefined
+      && this.data.myComment !== ""){
+      this.doComments();
+    }else{
+      wx.showToast({
+        title: '非法内容',
+        icon: 'error',
+        })
+    }
+  },
+  //邮箱失焦验证
+  validateEmail(e){
+    if(!this.checkEmail(e.detail.value)){
+      this.setData({
+        email:""
+      })
+    }
+  },
+  checkEmail(email){
+    let str = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/
+    if (str.test(email)) {
+      return true
+      } else {
+        wx.showToast({
+          title: '非法邮箱',
+          icon: 'error',
+          })
+          return false 
+     }  
+  },
+  doComments(){
+    const that = this;
+    console.log(this.data)
+    let params = {
+      "allowNotification": false,
+      "author": app.globalData.userInfo.nickName,
+      "authorUrl": app.globalData.userInfo.avatarUrl,
+      "content": this.data.myComment,
+      "email": this.data.email,
+      "parentId": this.data.currentComment === undefined ? 0 :this.data.currentComment.id,
+      "postId": this.data.articleId
+    }
+    console.log(params)
+    wx.request({
+      url: app.globalData.baseUrl + '/content/posts/comments?api_access_key='+app.globalData.api_access_key,
+      data:params,
+      method: 'POST',
+      header: 'Content-Type: application/json',
+      success: function (res) {
+        console.log(res)
+        if(res.data.status == 200){
+          that.loadComments(that.data.articleId)   
+          that.setData({actionSheetShow : false});
+          wx.showToast({
+            title: '评论成功',
+            icon: 'succcess',
+            })
+        }else{
+          wx.showToast({
+            title: '评论失败',
+            icon: 'error',
+            })
         }
       },
       fail: function (res) {
