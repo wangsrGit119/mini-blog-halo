fe6bac268eabba3cbf91903291a3a89a71939390
wangsrGit119 <1215618342@qq.com>
2021-10-10 20:20:32 +0800

    日志图片样式优化

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,8 +2,7 @@
 import deviceUtil from "./lin-ui/dist/utils/device-util"
 App({
 
-  // 引入`towxml3.0`解析方法
-	towxml:require('/towxml/index'),
+
   //自定义bar height
   capsuleBarHeight: deviceUtil.getNavigationBarHeight(),
   // 自定义tabbar
@@ -49,14 +48,14 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        env: 'suke-blog-dev-xxxxxxxxx',
+        env: 'suke-blog-dev-xxxxxxxxxxxx',
         traceUser: true,
       })
     }
 
     this.globalData = {
-      baseUrl: 'https://xxxxxxxxx.cn/api', //api
-      api_access_key:"xxxxxxxx", //token
+      baseUrl: 'https://xx.cn/api', //api
+      api_access_key:"xxx", //token
       index_bg_image_url:"https://cdn.jsdelivr.net/gh/wangsrGit119/wangsr-image-bucket/img-article/photo-1507738978512-35798112892c.jfif",//首页背景
       title:"Hi,I'm suke",//自定义title
       shareName:'suke的个人博客',//小程序分享名称
